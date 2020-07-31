9898fbe75ac457e73f3f0b2d461fd54cb7110a72
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-07-31 12:51:18 +0300

    add a function for the side navigation for sliding purposes when clicked by user

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// user interface
+//funtion for oprning and closing the side navigation bar
+function openSlideMenu(){
+  document.getElementById('menu').style.width = '250px';
+  document.getElementById('sidenav').style.marginLeft = '250px';
+}
+function closeSlideMenu(){
+  document.getElementById('menu').style.width = '0';
+  document.getElementById('sidenav').style.marginLeft = '0';
+}
